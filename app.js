0de19f77e69d8cc1b093491c88cba22595bc43dd
Raymond Jacobson <me@raymondjacobson.com>
2014-07-18 22:36:11 -0700

    add directories

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express')
+  , http = require('http')
+  , stylus = require('stylus')
+  , nib = require('nib')
+  , routes = require('routes.js')
+
+var app = express();
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+app.set('port', process.env.PORT || 1123);
+app.configure(function(){
+  app.use(express.bodyParser());
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  { src: __dirname + '/assets'
+  , compile: compile
+  }
+));
+app.use(express.static(__dirname + '/assets'))
+
+// handles
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express app listening on port ' + app.get('port'));
+});
