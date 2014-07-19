2a1e533602fbc12c5a332ff463b02ff1d7aa9dbd
Raymond Jacobson <me@raymondjacobson.com>
2014-07-19 00:06:09 -0700

    work on partials and angular

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function(){\
+  var myNamespace = {
+    myObject: {
+        sayWelcome: function() {
+            console.log("welcome to the "+this.theSeason + " of Hacks");
+        },
+        theSeason: "Summer"
+    }
+};
+var welcome = myNamespace.myObject();
+welcome.sayWelcome();
 })
